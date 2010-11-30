43c712b071e82753ce24e3e019ec6e1f6baee5f7
Alexander <yemelianov@mail.ru>
2010-11-30 12:05:58 +0200

    Changed error messages. Document posted after adding records.

--- AppliedSolutions/AWPAnalytics/Modules/data_processors/NavCsvImport/module.php
+++ AppliedSolutions/AWPAnalytics/Modules/data_processors/NavCsvImport/module.php
@@ -21,12 +21,13 @@ function onImport($event)
       throw new Exception('File not found');
    }
    
+   $errors    = array();
    $container = Container::getInstance();
    
    /* Create document */
-   
+   $doc_date = date('Y-m-d H:i:s');
    $document = $container->getModel('documents', 'ProjectTimeRecorder');
-   $params   = array('Date' => date('Y-m-d H:i:s'));
+   $params   = array('Date' => $doc_date);
    if (!$err = $document->fromArray($params)) $err = $document->save();
    
    if (empty($err))
@@ -35,11 +36,15 @@ function onImport($event)
       $tabular = $container->getModel('documents.ProjectTimeRecorder.tabulars', 'Records');
       $tabular->setAttribute('Owner', $document->getId());
    }
-   else $have_doc = false;
+   else
+   {
+      $have_doc = false;
+      $errors   = "Document not created.";
+   }
    
    /* Fill catalogs and document tabular section */
-   
-   $errors     = array();
+      
+   $rejected   = $container->getModel('information_registry', 'RejectedImportRecords');
    $employee   = $container->getModel('catalogs', 'Employees');
    $project    = $container->getModel('catalogs', 'Projects');
    $subproject = $container->getModel('catalogs', 'SubProjects');
@@ -61,6 +66,7 @@ function onImport($event)
       $row++;
       
       $add_in_tabular = true;
+      $rejectReason   = array();
       
       
       /* catalogs.Employees */
@@ -105,10 +111,11 @@ function onImport($event)
 
          if (!empty($err))
          {
-            $add_in_tabular = false;
+            $add_in_tabular      = false;
             $empl_dump[$data[4]] = 0;
-            $errors[$row]['catalogs'][$entity->getType()] = $err;
-            $errors[$row]['catalogs'][$entity->getType()][] = 'Name: '.$data[5];
+            $rejectReason        = array_merge($rejectReas, $err);
+            
+            $errors[] = "Employee not added: Code - '".$data[4]."', Name - '".$data[5]."'.";
          }
          else $empl_dump[$data[4]] = $entity->getId();
       }
@@ -132,10 +139,11 @@ function onImport($event)
 
          if (!empty($err))
          {
-            $add_in_tabular = false;
+            $add_in_tabular      = false;
             $area_dump[$data[6]] = 0;
-            $errors[$row]['catalogs'][$entity->getType()] = $err;
-
+            $rejectReason        = array_merge($rejectReas, $err);
+            
+            $errors[] = "Business Area not added: Code - '".$data[6]."'.";
          }
          else $area_dump[$data[6]] = $entity->getId();
       }
@@ -159,9 +167,11 @@ function onImport($event)
 
          if (!empty($err))
          {
-            $add_in_tabular = false;
+            $add_in_tabular      = false;
             $proj_dump[$data[0]] = 0;
-            $errors[$row]['catalogs'][$entity->getType()] = $err;
+            $rejectReason        = array_merge($rejectReas, $err);
+            
+            $errors[] = "Project not added: Code - '".$data[0]."', Name - '".$data[1]."'.";;
          }
          else $proj_dump[$data[0]] = $entity->getId();
       }
@@ -181,25 +191,31 @@ function onImport($event)
              
             if (empty($proj_dump[$data[0]]))
             {
-               $errors[$row]['catalogs'][$entity->getType()][] = 'Parent project not exists';
-               continue;
+               $add_in_tabular = false;
+               $rejectReason[] = 'Parent project not exists.';
+               
+               $errors[] = "SubProject not added: Code - '".$data[2]."', Name - '".$data[3]."', Project - '".$data[0]."'.";
             }
-             
-            $params = array(
-               'Code'        => $data[2],
-               'Description' => $data[3],
-               'Project'     => $proj_dump[$data[0]]
-            );
-
-            if (!$err = $entity->fromArray($params, array('replace' => true))) $err = $entity->save();
-
-            if (!empty($err))
+            else
             {
-               $subp_dump[$data[2]] = 0;
-               $errors[$row]['catalogs'][$entity->getType()] = $err;
-               continue;
+               $params = array(
+                  'Code'        => $data[2],
+                  'Description' => $data[3],
+                  'Project'     => $proj_dump[$data[0]]
+               );
+
+               if (!$err = $entity->fromArray($params, array('replace' => true))) $err = $entity->save();
+
+               if (!empty($err))
+               {
+                  $add_in_tabular      = false;
+                  $subp_dump[$data[2]] = 0;
+                  $rejectReason        = array_merge($rejectReas, $err);
+                  
+                  $errors[] = "SubProject not added: Code - '".$data[2]."', Name - '".$data[3]."', Project - '".$data[0]."'.";
+               }
+               else $subp_dump[$data[2]] = $entity->getId();
             }
-            else $subp_dump[$data[2]] = $entity->getId();
          }
          elseif (empty($subp_dump[$data[2]]))
          {
@@ -210,25 +226,75 @@ function onImport($event)
       
       /* Add current row in tabular section */
       
-      if (!$have_doc || !$add_in_tabular) continue;
+      if (!$add_in_tabular || !$have_doc)
+      {
+         $add_in_rejected = true;
+         
+         if (!$have_doc)
+         {
+            $rejectReason[] = 'Document not created.'; 
+         }
+      }
+      else
+      {
+         $add_in_rejected = false;
       
-      $params = array(
-         'Project'      => $proj_dump[$data[0]],
-         'SubProject'   => empty($data[2]) ? 0 : $subp_dump[$data[2]],
-         'Employee'     => $empl_dump[$data[4]],
-         'BusinessArea' => $area_dump[$data[6]],
-         'HoursSpent'   => str_replace(',', '.', $data[7])
-      );
-      $entity = clone $tabular;
+         $params = array(
+            'Project'      => $proj_dump[$data[0]],
+            'SubProject'   => empty($data[2]) ? 0 : $subp_dump[$data[2]],
+            'Employee'     => $empl_dump[$data[4]],
+            'BusinessArea' => $area_dump[$data[6]],
+            'HoursSpent'   => str_replace(',', '.', $data[7])
+         );
+         $entity = clone $tabular;
 
-      if (!$err = $entity->fromArray($params)) $err = $entity->save();
+         if (!$err = $entity->fromArray($params)) $err = $entity->save();
 
-      if (!empty($err)) $errors[$row][$entity->getKind()][$entity->getType()] = $err;
+         if (!empty($err))
+         {
+            $add_in_rejected = true;
+            $rejectReason    = $err;
+            
+            $errors[] = 'Row not added in tabular section.';
+         }
+      }
+      
+      
+      /* Add in Rejected */
+      
+      if ($add_in_rejected)
+      {
+         $ret = true;
+         $ir  = clone $rejected;
+         
+         $ret = $ret && $ir->setAttribute('Project', $data[0]);
+         $ret = $ret && $ir->setAttribute('SubProject', $data[2]);
+         $ret = $ret && $ir->setAttribute('Employee', $data[4]);
+         $ret = $ret && $ir->setAttribute('BusinessArea', $data[6]);
+         $ret = $ret && $ir->setAttribute('HoursSpent', str_replace(',', '.', $data[7]));
+         $ret = $ret && $ir->setAttribute('DocumentType', '');
+         $ret = $ret && $ir->setAttribute('DocumentNumber', 0);
+         $ret = $ret && $ir->setAttribute('Date', $doc_date);
+         $ret = $ret && $ir->setAttribute('RejectReason', implode('; ', $rejectReason).'.');
+
+         if ($ret)
+         {
+            if ($err = $ir->save()) $ret = false;
+         }
+         else $err[] = 'Some values have a wrong type';
+
+         if (!$ret) $errors[] = 'RejectedImportRecord not added.';
+      }
    }
    
    fclose($handle);
    
-   if (!empty($errors)) throw new Exception('<pre>'.print_r($errors, true).'</pre>');
+   if ($have_doc)
+   {
+      if ($err = $document->post()) $errors[] = 'Document not posted.';
+   } 
+   
+   if (!empty($errors)) throw new Exception(implode('<br/>', $errors));
    
    $event->setReturnValue(true);
 }
