968b65e3a7ed468d306c740f5ef38c43f54e4617
Alexander <yemelianov@mail.ru>
2010-12-09 18:14:11 +0200

    [AWPAnalytics] Updated Desktop

--- AppliedSolutions/AWPAnalytics/Resources/Desktop/Desktop.php
+++ AppliedSolutions/AWPAnalytics/Resources/Desktop/Desktop.php
@@ -1,5 +1,5 @@
 <div class="ae_import_desktop">
-  <img src="/ext/OEF/AppliedSolutions/AWPAnalytics/Resources/Desktop/AWP_Import_Process_Map.png" width="629" height="729" alt="Import Navigation" usemap="#ImportNavigation">
+  <img src="/ext/OEF/AppliedSolutions/AWPAnalytics/Resources/Desktop/AWP_Import_Process_Map.png" width="629" height="809" alt="Import Navigation" usemap="#ImportNavigation">
   <map name="ImportNavigation">
     <!-- Import -->
     <area shape="rect" coords="79,97, 199,187" href="{{ page.path..'/Perform Import' }}" alt="Perform Import">
@@ -8,14 +8,20 @@
     <!-- Reports -->
     <area shape="rect" coords="79,261, 199,351" href="{{ page.path..'/Report ManHours' }}" alt="Projects with ManHours">
     <area shape="rect" coords="457,261, 577,351" href="{{ page.path..'/Report Resources' }}" alt="Project Resources">
-    <!-- Catalogs -->
-    <area shape="poly" coords="79,428, 136,428, 160,451, 159,530, 79,530" href="{{ page.path..'/Catalog Projects' }}" alt="Catalog Projects">
-    <area shape="poly" coords="217,428, 274,428, 297,451, 297,530, 217,530" href="{{ page.path..'/Catalog SubProjects' }}" alt="Catalog SubProjects">
-    <area shape="poly" coords="358,428, 416,428, 438,451, 438,530, 358,530" href="{{ page.path..'/Catalog Employees' }}" alt="Catalog Employees">
-    <area shape="poly" coords="493,428, 552,428, 575,451, 575,530, 493,530" href="{{ page.path..'/Catalog BusinessAreas' }}" alt="Catalog Business Areas">
+	
+	<area shape="rect" coords="81,373, 156,463" href="{{ page.path..'/Report RegisteredProjects' }}" alt="Registered Projects">
+    <area shape="rect" coords="184,373, 259,463" href="{{ page.path..'/Report ProjectOverview' }}" alt="Project Overview">
+	<area shape="rect" coords="284,373, 363,463" href="{{ page.path..'/Report ResourceAssignments' }}" alt="Resource Assignments">
+    <area shape="rect" coords="389,373, 470,463" href="{{ page.path..'/Report ProjectPerformance' }}" alt="Project Performance">
+	<area shape="rect" coords="493,373, 572,463" href="{{ page.path..'/Report ResourceCost' }}" alt="Resource Cost">
+    <!-- Catalogs --> 
+    <area shape="poly" coords="79,519, 136,519, 160,541, 159,620, 79,620" href="{{ page.path..'/Catalog Projects' }}" alt="Catalog Projects">
+    <area shape="poly" coords="217,519, 274,519, 297,541, 297,620, 217,620" href="{{ page.path..'/Catalog SubProjects' }}" alt="Catalog SubProjects">
+    <area shape="poly" coords="358,519, 416,519, 438,541, 438,620, 358,620" href="{{ page.path..'/Catalog Employees' }}" alt="Catalog Employees">
+    <area shape="poly" coords="493,519, 552,519, 575,541, 575,620, 493,620" href="{{ page.path..'/Catalog BusinessAreas' }}" alt="Catalog Business Areas">
     <!-- Documents -->
-    <area shape="poly" coords="79,597, 136,597, 160,620, 159,700, 79,700" href="{{ page.path..'/Docs Project Registration' }}" alt="Project Registration">
-    <area shape="poly" coords="217,597, 274,597, 297,620, 297,700, 217,700" href="{{ page.path..'/Docs Project Assignment' }}" alt="Project Assignment">
-    <area shape="poly" coords="358,597, 416,597, 438,620, 438,700, 358,700" href="{{ page.path..'/Docs Time Recorder' }}" alt="Project Time Recorder">
+    <area shape="poly" coords="79,672, 136,672, 160,695, 159,774, 79,774" href="{{ page.path..'/Docs Project Registration' }}" alt="Project Registration">
+    <area shape="poly" coords="217,672, 274,672, 297,695, 297,774, 217,774" href="{{ page.path..'/Docs Project Assignment' }}" alt="Project Assignment">
+    <area shape="poly" coords="358,672, 416,672, 438,695, 438,774, 358,774" href="{{ page.path..'/Docs Time Recorder' }}" alt="Project Time Recorder">
   </map>
 </div>
