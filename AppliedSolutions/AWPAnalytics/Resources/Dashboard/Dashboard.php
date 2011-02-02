0fc643195518530e46e11c21a671c7de62ea5e4c
dresha <dresha48@gmail.com>
2011-02-02 13:47:05 +0200

    [AWPAnalytics] Employee Vacations Days

--- AppliedSolutions/AWPAnalytics/Resources/Dashboard/Dashboard.php
+++ AppliedSolutions/AWPAnalytics/Resources/Dashboard/Dashboard.php
@@ -2,77 +2,83 @@
 {{
    &lt;html&gt;
      &lt;head&gt;
+       <link href="/ext/OEF/AppliedSolutions/AWPAnalytics/MindTouch/CSS/widget.css" media="print" type="text/css" rel="stylesheet">
        &lt;script type="text/javascript" src="https://www.google.com/jsapi"&gt;&lt;/script&gt;
        &lt;script type="text/javascript" src="/ext/OEF/Framework/MindTouch/Js/oe_widgets.js"&gt;&lt;/script&gt;
        &lt;script type="text/javascript"&gt;"
          Widgets.showWidget({
 	        'load': {
 		       'solution': 'AWPAnalytics',
-		       'service':  'Pm',
-		       'method':   'ProjectCost',
+		       'service':  'Personal',
+		       'method':   'EmployeeHours',
 		       'authMethod': 'MTAuth',
 		       'authtoken' : '"..user.authtoken.."',
 		       'attributes': {
-		          'Project': '263'
+		          'Period': 'This Month'
 		       }
 	        },
 	        'view': {
-		       'widget':  'ColumnChart',
-		       'tag_id':  'oe_widget_3',
-		       'options': {
-				   width: 400,
-				   height: 240,
-				   title: 'Project cost',
-				   hAxis: {title: 'Date', titleTextStyle: {color: 'red'}}
-			   }
+		       'widget':  'Speedometer',
+		       'tag_id':  'oef_employee_hours',
+		       'options': {}
 	        }
          });
+
+         Widgets.showWidget({
+	        'load': {
+		       'solution': 'AWPAnalytics',
+		       'service':  'Personal',
+		       'method':   'EmployeeVacationDays',
+		       'authMethod': 'MTAuth',
+		       'authtoken' : '"..user.authtoken.."',
+	        },
+	        'view': {
+		       'widget':  'EmployeeVacationDays',
+		       'tag_id':  'oef_employee_vacation_days',
+		       'options': {}
+	        }
+         });
+
        "&lt;/script&gt;
      &lt;/head&gt;
      &lt;body&gt;&lt;/body&gt;
      &lt;tail&gt;
-       &lt;script type="text/javascript"&gt;"
+     
+            &lt;script type="text/javascript"&gt;"
          Widgets.showWidget({
 	        'load': {
 		       'solution': 'AWPAnalytics',
 		       'service':  'Pm',
-		       'method':   'UserProjects',
-		       'authMethod': 'MTAuth',
-		       'authtoken' : '"..user.authtoken.."',
-		       'attributes': {}
-	        },
-	        'view': {
-		       'widget':  'Grid',
-		       'tag_id':  'oe_widget_1',
-		       'options': {}
-	        }
-         });
-         Widgets.showWidget({
-	        'load': {
-		       'solution': 'AWPAnalytics',
-		       'service':  'Pm',
-		       'method':   'ProjectMembers',
+		       'method':   'ProjectOverview',
 		       'authMethod': 'MTAuth',
 		       'authtoken' : '"..user.authtoken.."',
 		       'attributes': {
-		          'Project': '263'
+		          'Project': '00000001'
 		       }
 	        },
 	        'view': {
-		       'widget':  'Grid',
-		       'tag_id':  'oe_widget_2',
+		       'widget':  'ProjectOverview',
+		       'tag_id':  'oef_project_overview',
 		       'options': {}
 	        }
          });
+
        "&lt;/script&gt;
+     
      &lt;/tail&gt;
    &lt;/html&gt;
 }}
 <div class="oef_content">
-  <h3>UserProjects</h3>
-  <div id="oe_widget_1">&nbsp;</div>
-  <h3>ProjectMembers</h3>
-  <div id="oe_widget_2">&nbsp;</div>
-  <h3>ProjectCost</h3>
-  <div id="oe_widget_3">&nbsp;</div>
+  <h3>Employee Hours</h3>
+
+  <div id="oef_employee_hours"><div id='chart1_div' style="float: left;">&nbsp;</div><div id='chart2_div' style="float: left;">&nbsp;</div><div id='chart3_div' style="float: left;">&nbsp;</div></div>
+
+</div>
+<div style="clear:both">
+
+<h3 >Employee Vacations Days</h3>
+  <div id="oef_employee_vacation_days"><div id='chart' style="float: left;">&nbsp;</div><div id='table_chart' style="float: left;">&nbsp;</div></div>
+    
+      <h3>Project Overview</h3>
+<div id="oef_project_overview">&nbsp;</div>
 </div>
