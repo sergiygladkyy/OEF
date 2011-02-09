05dad29a2870d2c609bed67f0cef803f0c4f661d
dresha <dresha48@gmail.com>
2011-02-09 11:44:30 +0200

    [AWP Analitics] dashboard widgets fix

--- AppliedSolutions/AWPAnalytics/Templates/Widgets/Dashboard/DashboardHorizontal.php
+++ AppliedSolutions/AWPAnalytics/Templates/Widgets/Dashboard/DashboardHorizontal.php
@@ -1,11 +1,15 @@
-<div>
-	<div id="zone_0" style="float: left;">&nbsp;</div>
-	<div id="zone_1" style="float: left;">&nbsp;</div>
-	<div id="zone_2" style="float: left;">&nbsp;</div>
-	<div id="zone_3" style="float: left;">&nbsp;</div>
-	<div id="zone_4" style="float: left;">&nbsp;</div>
-	<div id="zone_5" style="float: left;">&nbsp;</div>
-	<div id="zone_6" style="float: left;">&nbsp;</div>
-	<div id="zone_7" style="float: left;">&nbsp;</div>
-	<div id="zone_8" style="float: left;">&nbsp;</div>
-</div>
+<table border="0" cellpadding="1" cellspacing="1" style="table-layout: fixed; width: 100%;">
+	<tbody>
+		<tr>
+			<td>
+				<div id="zone_0" style="float: left;">&nbsp;</div>
+			</td>
+			<td>
+				<div id="zone_1" style="float: left;">&nbsp;</div>
+			</td>
+			<td>
+				<div id="zone_2" style="float: left;">&nbsp;</div>
+			</td>
+		</tr>
+	</tbody>
+</table>
--- AppliedSolutions/AWPAnalytics/Templates/Widgets/Dashboard/DashboardVertical.php
+++ AppliedSolutions/AWPAnalytics/Templates/Widgets/Dashboard/DashboardVertical.php
@@ -8,4 +8,13 @@
 	<div id="zone_6">&nbsp;</div>
 	<div id="zone_7">&nbsp;</div>
 	<div id="zone_8">&nbsp;</div>
+        <div id="zone_9">&nbsp;</div>
+	<div id="zone_10">&nbsp;</div>
+	<div id="zone_11">&nbsp;</div>
+	<div id="zone_12">&nbsp;</div>
+	<div id="zone_13">&nbsp;</div>
+	<div id="zone_14">&nbsp;</div>
+	<div id="zone_15">&nbsp;</div>
+	<div id="zone_16">&nbsp;</div>
+	<div id="zone_17">&nbsp;</div>
 </div>
