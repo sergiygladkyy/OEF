484e331a04d4331483c93b3b986dd5cafdc98007
Alexander <yemelianov@mail.ru>
2011-02-14 15:02:24 +0200

    [AWPAnalytics] Updated report TimeCards (hours from AccumulationRegisters.EmployeeHoursReported)

--- AppliedSolutions/AWPAnalytics/Modules/reports/TimeCards/module.php
+++ AppliedSolutions/AWPAnalytics/Modules/reports/TimeCards/module.php
@@ -17,57 +17,116 @@ function onGenerate($event)
    if (!empty($headline['Employee']))
    {
       $employee = (int) $headline['Employee'];
-      $criterion[0] = 'tc.`Employee` = '.$employee;
+      $criterion[0] = '`Employee` = '.$employee;
    }
    
    if (!empty($headline['Period']))
    {
       $period  = MGlobal::parseDatePeriodString($headline['Period']);
       $header .= ' from '.date('d.m.Y', strtotime($period[0]));
-      $criterion[1] = "tc.`StartDate` >= '".$period[0]."'";
+      $criterion[1] = "`StartDate` >= '".$period[0]."'";
       
       if (isset($period[1]))
       {
          $header .= ' to '.date('d.m.Y', strtotime($period[1]));
-         $criterion[2] = "tc.`EndDate` < '".$period[1]."'";
+         $criterion[2] = "`EndDate` < '".$period[1]."'";
       }
    }
    
    $odb = $container->getODBManager();
-   $query = "SELECT tc._id, tc.`Employee`, tc.`StartDate`, tc.`EndDate`, tc.`_post` AS `Posted`, SUM(tr.`Hours`) AS `Hours` ". 
-            "FROM `documents`.`TimeCard` AS `tc`, `documents`.`TimeCard`.`tabulars`.`TimeRecords` AS `tr` ".
-            "WHERE ".($criterion ? implode(' AND ', $criterion).' AND ' : '')." tc._id = tr.Owner ".
-            "GROUP BY tr.Owner ".
-            "ORDER BY tc.`Employee`, tc.`StartDate`";
+   $query = "SELECT `_id`, `Employee`, `StartDate`, `EndDate`, `_post` AS `Posted` ". 
+            "FROM `documents`.`TimeCard` ".
+            ($criterion ? 'WHERE '.implode(' AND ', $criterion).' ' : '').
+            "ORDER BY `Employee`, `StartDate`";
    
    if (null === ($res = $odb->executeQuery($query)))
    {
       throw new Exception('Database error');
    }
    
-   $ids   = array();
-   $cards = array();
-   $links = array();
-   $total = 0;
+   $empIDS  = array();
+   $periods = array();
+   $docs    = array();
+   $totals  = array();
    
    while ($row = $odb->fetchAssoc($res))
    {
-      $cards[$row['_id']] = array(
+      $docs[$row['_id']] = $row;
+      
+      $empIDS[$row['Employee']] = $row['Employee'];
+      
+      if ($row['Posted'] == '1')
+      {
+         $periods[$row['StartDate']][$row['EndDate']][$row['Employee']] = $row['Employee'];
+      }
+   }
+   
+   if (!empty($periods))
+   {
+      $cmodel = $container->getCModel('AccumulationRegisters', 'EmployeeHoursReported');
+       
+      foreach ($periods as $start => $param)
+      {
+         foreach ($param as $end => $ids)
+         {
+            $total = $cmodel->getTotals(array($start, date('Y-m-d', strtotime($end) + 86400)), array('criteria' => array('Employee' => $ids)));
+            
+            foreach ($total as $row)
+            {
+               if (isset($totals[$start][$end][$row['Employee']]))
+               {
+                  $totals[$start][$end][$row['Employee']]['Hours']    += $row['Hours'];
+                  $totals[$start][$end][$row['Employee']]['Overtime'] += $row['OvertimeHours'];
+                  $totals[$start][$end][$row['Employee']]['Extra']    += $row['ExtraHours'];
+               }
+               else
+               {
+                  $totals[$start][$end][$row['Employee']] = array(
+                     'Hours'    => $row['Hours'],
+                     'Overtime' => $row['OvertimeHours'],
+                     'Extra'    => $row['ExtraHours']
+                  );
+               }
+            }
+         }
+      }
+      
+      unset($periods);
+   }
+   
+   $cards = array();
+   $links = array();
+   $total = array('Hours' => 0, 'Overtime' => 0, 'Extra' => 0);
+   
+   foreach ($docs as $id => $row)
+   {
+      $cards[$id] = array(
          'Employee' => $row['Employee'],
          'Period'   => date('d.m.Y', strtotime($row['StartDate'])).' - '.date('d.m.Y', strtotime($row['EndDate'])),
          'Posted'   => $row['Posted'],
-         'Hours'    => $row['Hours']
+         'Hours'    => 0,
+         'Overtime' => 0,
+         'Extra'    => 0
       );
       
-      $total += $row['Hours'];
-      
-      $ids[$row['Employee']] = $row['Employee'];
+      if ($row['Posted'] == '1' && isset($totals[$row['StartDate']][$row['EndDate']][$row['Employee']]))
+      {
+         $tot =& $totals[$row['StartDate']][$row['EndDate']][$row['Employee']];
+         
+         $cards[$id]['Hours']    = $tot['Hours'];
+         $cards[$id]['Overtime'] = $tot['Overtime'];
+         $cards[$id]['Extra']    = $tot['Extra'];
+         
+         $total['Hours']    += $tot['Hours'];
+         $total['Overtime'] += $tot['Overtime'];
+         $total['Extra']    += $tot['Extra'];
+      }
    }
    
-   if ($ids)
+   if ($empIDS)
    {
       $cmodel = $container->getCModel('catalogs', 'Employees');
-      $links  = $cmodel->retrieveLinkData($ids);
+      $links  = $cmodel->retrieveLinkData($empIDS);
    }  
    
    
@@ -99,9 +158,11 @@ function onGenerate($event)
          $report->put($a_employee);
       }
       
-      $a_card->parameters['Period'] = $row['Period'];
-      $a_card->parameters['Posted'] = $row['Posted'] == '1' ? 'yes' : 'no';
-      $a_card->parameters['Hours']  = $row['Hours'];
+      $a_card->parameters['Period']   = $row['Period'];
+      $a_card->parameters['Posted']   = $row['Posted'] == '1' ? 'posted' : 'no posted';
+      $a_card->parameters['Hours']    = $row['Hours'];
+      $a_card->parameters['Overtime'] = $row['Overtime'];
+      $a_card->parameters['Extra']    = $row['Extra'];
       
       $a_card->decode['TimeCard'] = array(
          'uid'     => 'documents.TimeCard',
@@ -113,7 +174,9 @@ function onGenerate($event)
    }
    
    $area = $mockup->getArea('total');
-   $area->parameters['OverallHours'] = $total;
+   $area->parameters['THours']    = $total['Hours'];
+   $area->parameters['TOvertime'] = $total['Overtime'];
+   $area->parameters['TExtra']    = $total['Extra'];
    
    $report->put($area);
    
