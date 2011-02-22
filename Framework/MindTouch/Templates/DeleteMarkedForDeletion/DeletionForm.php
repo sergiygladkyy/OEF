55309f16b5e95c3710ae58fe0e384c0d24f4849c
Alexander <yemelianov@mail.ru>
2011-02-22 15:00:13 +0200

    [MTTemplates] Fixed bug in DeletionForm template

--- Framework/MindTouch/Templates/DeleteMarkedForDeletion/DeletionForm.php
+++ Framework/MindTouch/Templates/DeleteMarkedForDeletion/DeletionForm.php
@@ -40,31 +40,36 @@
   <form method="post" action="#" id="{{ class..'_form' }}">
     <div id="oef_marked_for_deletion_container">
       <table>
-      <eval:foreach var="kind" in="map.keys(data)">
-        <tr>
-          <td colspan="2" class="oef_group_header">{{ string.toupperfirst(kind) }}</td>
-        </tr>
-        {{ var types = data[kind] }}
-        <eval:foreach var="type" in="map.keys(types)">
-          {{
-             var params = types[type];
-             var name_prefix = 'aeform['..kind..']['..type..']';
-             var cnt = 0;
-          }}
-          <eval:foreach var="row" in="params">
-            {{ var linkclass = 'oef_link oef_'..kind..'_'..type..'_'..row['value']; }}
-            <tr class="{{ 'oef_marked_item oef_'..kind..'_'..type..'_item' }}">
-              <td class="{{ 'oef_deletion_checkboxes'..(cnt % 2 == 0 ? ' oef_even' : ' oef_not_even') }}">
-                <input type="checkbox" name="{{ name_prefix..'[]' }}" value="{{ row['value'] ?? 0 }}" checked />
-              </td>
-              <td class="{{ 'oef_deletion_links'..(cnt % 2 == 0 ? ' oef_even' : ' oef_not_even') }}">
-                <a href="{{ page.path..'?uid='..kind..'.'..type..'&actions=displayItemForm&id='..row['value']}}" target="_blank" class="{{ linkclass }}">{{ row['text'] }}</a>
-              </td>
-            </tr>
-            {{ let cnt += 1 }}
+      <eval:if test="data is map">
+        <eval:foreach var="kind" in="map.keys(data)">
+          <tr>
+            <td colspan="2" class="oef_group_header">{{ string.toupperfirst(kind) }}</td>
+          </tr>
+          {{ var types = data[kind] }}
+          <eval:foreach var="type" in="map.keys(types)">
+            {{
+               var params = types[type];
+               var name_prefix = 'aeform['..kind..']['..type..']';
+               var cnt = 0;
+            }}
+            <eval:foreach var="row" in="params">
+              {{ var linkclass = 'oef_link oef_'..kind..'_'..type..'_'..row['value']; }}
+              <tr class="{{ 'oef_marked_item oef_'..kind..'_'..type..'_item' }}">
+                <td class="{{ 'oef_deletion_checkboxes'..(cnt % 2 == 0 ? ' oef_even' : ' oef_not_even') }}">
+                  <input type="checkbox" name="{{ name_prefix..'[]' }}" value="{{ row['value'] ?? 0 }}" checked />
+                </td>
+                <td class="{{ 'oef_deletion_links'..(cnt % 2 == 0 ? ' oef_even' : ' oef_not_even') }}">
+                  <a href="{{ page.path..'?uid='..kind..'.'..type..'&actions=displayItemForm&id='..row['value']}}" target="_blank" class="{{ linkclass }}">{{ row['text'] }}</a>
+                </td>
+              </tr>
+              {{ let cnt += 1 }}
+            </eval:foreach>
           </eval:foreach>
         </eval:foreach>
-      </eval:foreach>
+      </eval:if>
+      <eval:else>
+        {{ let data = false }}
+      </eval:else>
       </table>
     </div>
     <div id="oef_related_entities_container">
@@ -85,6 +90,9 @@
        &lt;/head&gt;
        &lt;body&gt;&lt;/body&gt;
        &lt;tail&gt;
+       if (data == false) {
+         &lt;script type="text/javascript"&gt;"disabledForm('#"..class.."_form');"&lt;/script&gt; 
+       }
        &lt;/tail&gt;
      &lt;/html&gt;
   }}
