5a0d70388b791230b20132d6038c49c234837805
Alexander <yemelianov@mail.ru>
2011-03-25 18:23:53 +0200

    [OiltecIntranet] Implemented onProcess for custom form

--- AppliedSolutions/OiltecIntranet/Modules/catalogs/Employees/forms/UserProfile.php
+++ AppliedSolutions/OiltecIntranet/Modules/catalogs/Employees/forms/UserProfile.php
@@ -8,6 +8,8 @@
 function onGenerate($event)
 {
    $subject = $event->getSubject();
+   $kind    = $subject->getKind();
+   $type    = $subject->getType();
    $name    = $event['name'];
    $params  = $event['parameters'];
    $attrs   = array(
@@ -15,12 +17,14 @@ function onGenerate($event)
       'Person'      => array(),
       'StaffRecord' => array()
    );
-   $kind = $subject->getKind();
-   $type = $subject->getType();
-   $uploadDir=Utility::getUploadDir($kind, 'NaturalPersons', 'Photo');
-   $form_prefix = 'aeform['.$subject->getKind().']['.$subject->getType().']';
+   
+   $uploadDir   = Utility::getUploadDir($kind, 'NaturalPersons', 'Photo');
+   $form_prefix = 'aeform['.$kind.']['.$type.']';
    $container   = Container::getInstance();
-
+   
+   $gender = $container->getConfigManager()->getInternalConfigurationByKind('catalogs.field_prec', 'NaturalPersons');
+   $gender = $gender['Gender']['in'];
+    
    // Retrieve employee
    $empId    = !empty($params['employee']) ? (int) $params['employee'] : 0;
    $employee = $container->getModel('catalogs', 'Employees');
@@ -78,9 +82,27 @@ function onProcess($event)
    $errors = array();
    $values = $event['values'];
 
-   $event->setReturnValue(array(
-      'status'   => false,
-      'result'   => array('msg' => 'Not implemented<br><pre>'.print_r($values,true).'</pre>'),
-      'errors'   => $errors
-   ));
+   if (empty($values['_id']) || !is_numeric($values['_id']) || 0 >= (int) $values['_id'])
+   {
+      $event->setReturnValue(array('status' => false, 'result' => array('msg' => 'Unknow employee')));
+      return;
+   }
+   
+   $id = (int) $values['_id'];
+   
+   import('lib.model.base.BaseModel');
+   
+   if (!BaseModel::hasEntity('catalogs', 'NaturalPersons', $id))
+   {
+      $event->setReturnValue(array('status' => false, 'result' => array('msg' => 'Employee not exists')));
+      return;
+   }
+   
+   $controller = Container::getInstance()->getController('catalogs', 'NaturalPersons');
+      
+   $return = $controller->update($values);
+   
+   if ($return['status']) unset($return['result']['_id']);
+   
+   $event->setReturnValue($return);
 }
