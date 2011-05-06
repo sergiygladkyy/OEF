62209aa17255eb503ea3de19f86ab031d5a5c694
Alexander <yemelianov@mail.ru>
2011-05-06 14:05:57 +0300

    [OiltecIntranet] Changed EmployeeVacationDays widget

--- AppliedSolutions/OiltecIntranet/MindTouch/Templates/Widgets/EmployeeVacationDays.php
+++ AppliedSolutions/OiltecIntranet/MindTouch/Templates/Widgets/EmployeeVacationDays.php
@@ -7,7 +7,12 @@
       &lt;script type="text/javascript" src="/ext/OEF/Framework/MindTouch/Js/oe_widgets.js"&gt;&lt;/script&gt;
       &lt;script type="text/javascript"&gt;"
         jQuery(document).ready(function() {
-            jQuery('#"..zone.."').html('&lt;div id=\"oef_employee_vacation_days\"&gt;&lt;div id=\"chart\" style=\"float: left;\"&gt;&nbsp;&lt;/div&gt;&lt;div id=\"table_chart\" style=\"float: left;\"&gt;&nbsp;&lt;/div&gt;&lt;div style=\"clear:both\"&gt;&nbsp;&lt;/div&gt;&lt;/div&gt;');
+            var html = '';
+            html += '&lt;style&gt;#oef_employee_vacation_days .google-visualization-table-table { background-color: #f8f8f8; }';
+            html += '#oef_employee_vacation_days .google-visualization-table-th { background-image: none; background-color: #dfeddc; color: #84aa05; font-weight: bold; }&lt;/style&gt;';
+            html += '&lt;div id=\"oef_employee_vacation_days\"&gt;&lt;div id=\"chart\" style=\"float: left; margin-bottom: 10px;\"&gt;&nbsp;&lt;/div&gt;&lt;div id=\"table_chart\" style=\"float: left; margin-left: 9px;\"&gt;&nbsp;&lt;/div&gt;&lt;div style=\"clear:both\"&gt;&nbsp;&lt;/div&gt;&lt;/div&gt;';
+            
+            jQuery('#"..zone.."').html(html);
         });
         
 		Widgets.showWidget({
@@ -19,10 +24,18 @@
 		       'authtoken' : '"..user.authtoken.."',
 	        },
 	        'view': {
-		       'widget':  'EmployeeVacationDays',
-		       'tag_id':  'oef_employee_vacation_days',
+		       'widget': 'EmployeeVacationDays',
+		       'tag_id': 'oef_employee_vacation_days',
 		       'options': {
-		          header: 'Employee Vacations Days'
+		          header: 'Employee Vacations Days',
+		          width:  500,
+		          chart: {
+		             background: 'F8F8F8'
+		          },
+		          table: {
+		             showRowNumber: false,
+		             sort: 'disable'
+		          }
 		       }
 	        }
          }); 
